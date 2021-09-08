5838ba626e2f96667cb00256f108182215ffb92d
Swasthik <swasthik.ks22@gmail.com>
2021-09-08 23:03:40 +0530

    Updated Deck of Cards and used 2D array to print cards

--- src/DeckOfCards/DeckOfCards.java
+++ src/DeckOfCards/DeckOfCards.java
@@ -6,38 +6,25 @@ public class DeckOfCards {
 
 	public static void main(String[] args) {
 		Deck deck = new Deck();
-		ArrayList<Card> cards = deck.getCards();
+		ArrayList<String> cards = deck.getCards();
 		distributeCards(cards, 4, 9);
 	}
 
 	// Method to distribute the cards among players
-	public static void distributeCards(ArrayList<Card> cards, int numOfPlayer, int numOfCards) {
+	public static void distributeCards(ArrayList<String> cards, int numOfPlayer, int numOfCards) {
 
+		String[][] array2D = new String[numOfPlayer][numOfCards];
 		int k = 0;
 
-		for (int i = 1; i <= numOfPlayer; i++) {
-			System.out.println("Player " + i);
-			for (int j = 1; j <= numOfCards; j++) {
-				System.out.println(cards.get(k));
+		for (int i = 0; i < numOfPlayer; i++) {
+			System.out.println("\nPlayer " + (i+1));
+			for (int j = 0; j < numOfCards; j++) {
+				
+				array2D[i][j] = cards.get(k);
 				k++;
+				System.out.println(array2D[i][j]);
 			}
 		}
-	}
-}
-
-class Card {
-
-	private final String suit;
-	private final String rank;
-
-	public Card(String suit, String rank) {
-		this.suit = suit;
-		this.rank = rank;
-	}
-
-	@Override
-	public String toString() {
-		return "Card [suit=" + suit + ", rank=" + rank + "]";
 	}
 }
 
@@ -45,22 +32,22 @@ class Deck {
 
 	private static final String suits[] = { "Club", "Diamond", "Heart", "Spade" };
 	private static final String ranks[] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen","King" };
-	private final ArrayList<Card> cards;
+	private final ArrayList<String> cards;
 
 	//Creates a deck of all cards in sorted order
 	public Deck() {
-		cards = new ArrayList<Card>();
+		cards = new ArrayList<String>();
 		for (int i = 0; i < suits.length; i++) {
 			for (int j = 0; j < ranks.length; j++) {
-				this.cards.add(new Card(suits[i], ranks[j]));
+				this.cards.add(suits[i] + " - " + ranks[j]);
 			}
 		}
 
 		// Shuffle the deck
 		for (int i = 0; i < cards.size(); i++) {
 			int index = (int) (Math.random() * cards.size());
-			Card temp1 = cards.get(i);
-			Card temp2 = cards.get(index);
+			String temp1 = cards.get(i);
+			String temp2 = cards.get(index);
 			cards.set(i, temp2);
 			cards.set(index, temp1);
 		}
@@ -68,7 +55,7 @@ class Deck {
 	}
 
 	// Get the cards in the deck
-	public ArrayList<Card> getCards() {
+	public ArrayList<String> getCards() {
 		return cards;
 	}
 }
